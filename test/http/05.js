de97a408ad6eae3e3d0c7cbde8bb87a0667bfd97
unknown <1799864155@qq.com>
2019-05-19 19:53:26 +0800

    返回静态资源

--- test/http/05.js
+++ test/http/05.js
@@ -1,21 +1,28 @@
-var http=require('http');
-var fs=require('fs');
-var server=http.createServer();
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
 
-server.on('request',function(res,resp){
-    if(res.method=='GET'){
-        console.log('GET请求方式')
-    }else if(res.method=='POST'){
-        console.log('POST请求方式')
+server.on('request', function (res, resp) {
+    var urls = res.url;
+    console.log('请求路径：' + urls)
+    if (urls == '/') {
+        resp.setHeader('Content-Type', 'text/html;charset=utf-8')
+        fs.readFile('./index01.html', function (err, data) {
+            if (err) throw err;
+            console.log('读取文件成功');
+            resp.end(data);
+        })
+    } else {
+        //响应一切http请求的静态资源
+        fs.readFile('.' + urls, function (err, data) {
+            if (err) throw err;
+            console.log('读取图片成功');
+            resp.end(data)
+        })
     }
-    resp.setHeader('Content-Type', 'text/html;charset=utf-8')
-    fs.readFile('./index01.html',function(err,data){
-        if (err) throw err;
-        console.log('读取文件成功');
-        resp.end(data);
-    })
+
 })
 
-server.listen(8080,function(){
+server.listen(8080, function () {
     console.log('请访问http://localhost:8080')
 })
